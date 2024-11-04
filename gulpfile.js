58a503b75a6cea6d4db32775f87e1e6f2ad89b80
Daniel Freitas Ferreira <danielfferreirabr@gmail.com>
2024-11-04 16:48:57 -0300

    Ajustando as tarefas gulp

--- gulpfile.js
+++ gulpfile.js
@@ -6,10 +6,12 @@ import uglify from 'gulp-uglify';
 import imagemin from 'gulp-imagemin';
 import concat from 'gulp-concat';
 import autoprefixer from 'gulp-autoprefixer';
+import browserSync from 'browser-sync';
 
 import * as sass from 'sass';
 
 const compileSass = gulpSass(sass);
+const server = browserSync.create();
 
 const paths = {
   html: 'src/*.html',
@@ -20,13 +22,31 @@ const paths = {
   dist: 'dist'
 };
 
+function serve(done) {
+  server.init({
+    server: {
+      baseDir: './'
+    }
+  });
+  done();
+}
+
+
+function reload(done) {
+  server.reload();
+  done();
+}
+
+
 gulp.task('html', function() {
   return gulp.src(paths.html)
     .pipe(fileInclude({
       prefix: '@@',
       basepath: '@file'
     }))
-    .pipe(gulp.dest(paths.dist));
+    // .pipe(gulp.dest(paths.dist))
+    .pipe(gulp.dest('.'))
+    .pipe(server.stream());
 });
 
 
@@ -35,7 +55,9 @@ gulp.task('styles', function() {
     .pipe(compileSass().on('error', compileSass.logError)) 
     .pipe(autoprefixer()) 
     .pipe(cleanCSS()) 
-    .pipe(gulp.dest(`${paths.dist}/css`)); 
+    // .pipe(gulp.dest(`${paths.dist}/css`))
+    .pipe(gulp.dest('css'))
+    .pipe(server.stream());
 });
 
 
@@ -43,23 +65,27 @@ gulp.task('scripts', function() {
   return gulp.src(paths.scripts) 
     .pipe(concat('main.js')) 
     .pipe(uglify()) 
-    .pipe(gulp.dest(`${paths.dist}/js`)); 
+    // .pipe(gulp.dest(`${paths.dist}/js`))
+    .pipe(gulp.dest('js'))
+    .pipe(server.stream()); 
 });
 
 
 gulp.task('images', function() {
   return gulp.src(paths.images) 
     .pipe(imagemin())
-    .pipe(gulp.dest(`${paths.dist}/images`)); 
+    // .pipe(gulp.dest(`${paths.dist}/images`))
+    .pipe(gulp.dest('images'))
+    .pipe(server.stream()); 
 });
 
 
 gulp.task('watch', function() {
-  gulp.watch([paths.html, paths.components], gulp.series('html')); 
-  gulp.watch('src/styles/**/*.scss', gulp.series('styles')); 
-  gulp.watch(paths.scripts, gulp.series('scripts')); 
-  gulp.watch(paths.images, gulp.series('images')); 
+  gulp.watch([paths.html, paths.components], gulp.series('html', reload)); 
+  gulp.watch('src/styles/**/*.scss', gulp.series('styles', reload)); 
+  gulp.watch(paths.scripts, gulp.series('scripts', reload)); 
+  gulp.watch(paths.images, gulp.series('images', reload)); 
 });
 
 
-gulp.task('default', gulp.series('html', 'styles', 'scripts', 'images', 'watch'));
+gulp.task('default', gulp.series('html', 'styles', 'scripts', 'images', serve, 'watch'));
